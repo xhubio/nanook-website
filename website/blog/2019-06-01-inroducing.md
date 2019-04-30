7204cf39574bf193f64495750eb7a8cb4d9453e7
Torsten Link <torstenlink@gmx.de>
2019-04-30 23:06:22 +0200

    docs(blog, api): readable API and Blog.

---
+++ website/blog/2019-06-01-inroducing.md
@@ -0,0 +1,7 @@
+---
+title: Introducing Nanook-Table
+author: Torsten Link
+---
+
+We are proud to introduce Nanook-Table. This is the first 'official' version of Nanook-Table. It has been used about
+one year now in a productive environment.
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -56,11 +56,12 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'tutorials/overview', label: 'Docs'},
+    {doc: 'api/logger/LoggerInterface', label: 'API'},
     /* you can add external links */
+    {blog: true, label: 'Blog'},
     {href: repoUrl, label: 'GitHub'},
     // {doc: 'api/overview', label: 'API'},
     // {page: 'help', label: 'Help'},
-    // {blog: true, label: 'Blog'},
   ],
 
   // If you have users set above, you add it here:
